ab3485d3d73806378339d8a6a9ec02e893beca47
Stuart Hicks <stuart.hicks@gmail.com>
2019-07-03 22:14:30 +0200

    Scaffolding
    Creating all the pages and components in line with the plan.

---
+++ src/components/issuecard.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+  <p>This is an issue</p>
+)
---
+++ src/components/menu.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => (
+  <p>May I see the menu please?</p>
+)
