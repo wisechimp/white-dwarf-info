0d42de1c55d6e5b59af83b4261e135d260ff8c62
Stuart Hicks <stuart.hicks@gmail.com>
2019-07-04 10:31:37 +0200

    The links page

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,5 +1,26 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+
 export default () => (
-  <h1>Links</h1>
+  <Layout pageTitle="My Links">
+    <h1>My Links</h1>
+    <p>
+There are approximately 50 gazillion websites with information, opinions, rumours, intelligent debate, unintelligent debate and news of varying qualities of <a href="http://en.wikipedia.org/wiki/Truthiness" >truthiness</a> regarding the Warhammer hobby and it's various friends and relations. However I have my favourites and, in no particular order, here they are for you information and interest.
+</p>
+
+<ul>
+<li><a href="http://www.games-workshop.com/gws/home.jsp">Games Workshop</a> - These guys have some sort of relation ship with the whole wargaming thing I've heard...</li>
+<li><a href="http://www.forgeworld.co.uk" >Forgeworld</a> - As if Games Workshop didn't make enough minatures their companion site have some fine sculpts available for the more serious (and wealthy!) wargamer.</li>
+<li><a href="https://www.blacklibrary.com/">The Black Library</a> - The publishing arm of Games Workshop who generate a fantastic volume of fictional background for the Warhammer worlds.</li>
+<li><a href="http://www.ebay.co.uk/">ebay</a> - The best place to locate old magazines or miniatures, although prices have gone up since I were a lad!</li>
+<li><a href="http://index.rpg.net/display-search.phtml?key=magazine&amp;value=White+Dwarf&amp;type=pictures">White Dwarf Index: 1-100</a>: An index of the first 100 issues of White Dwarf</li>
+<li><a href="http://www.enworld.org/forum/showthread.php?325009-White-Dwarf-The-First-100-issues-A-Read-Through-and-Review" >White Dwarf Review: 1-100</a> - A wonderfully descriptive journey through the first 100 issues of White Dwarf from a roleplay perspective, posted on the EN World forum.</li>
+<li><a href="http://www.gitzmansgallery.com/shdmotwow.html" >Warhammer World Map</a> - A simply stunning Warhammer world Google map.</li>
+<li><a href="http://www.warhammer-empire.com/theforum/" >Warhammer Empire Forum</a> - An interesting forum for WFB Empires players.</li>
+<li><a href="http://generalsimiussapiens.blogspot.co.uk" >Simius Sapiens Blog</a> - My stuttering attempt at a wargaming blog! It follows an aging Empire general getting to grips with the 8th edition (whatever that was!).</li>
+<li><a href="http://realmofchaos80s.blogspot.co.uk" >Realm of Chaos 80s</a> - A very nostalgic blog covering everything Warhammer Fantasy related back in the 80s, plus articles on the state of the hobby in general. The blog includes interviews with many famous names involved with Games Workshop at the time.</li>
+<li><a href="http://www.solegends.com/index.htm" >Stuff of Legends</a> - Effectively an online museum for the fantasy and sci-fi gaming hobby with a myriad of images of minatures.</li>
+</ul>
+  </Layout>
 )
