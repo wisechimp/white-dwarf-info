8a8e310a4a120bc8ed681f8915f896d40253b95e
Stuart Hicks <stuart.hicks@gmail.com>
2019-07-04 11:06:51 +0200

    Issue card created
    Issue card created with props and styles. Successfully set up one on the issue page using static data.

--- src/components/issuecard.js
+++ src/components/issuecard.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => (
-  <p>This is an issue</p>
+import styles from "./issuecard.module.css"
+
+export default (props) => (
+  <div className={styles.issueCard}>
+    <img src={props.issueCoverSrc} />
+    <div className={styles.issueCardText}>
+      <Link to={props.issueLink}>{props.issueNumber}</Link>
+      <p>{props.issueDate}</p>
+    </div>
+  </div>
 )
--- src/pages/issues.js
+++ src/pages/issues.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+import Cover from '../data/images/tn_white-dwarf-magazine-cover-101.jpg'
+import IssueCard from '../components/issuecard'
+import Layout from '../components/layout'
+
 export default () => (
-  <h1>Issues</h1>
+  <Layout pageTitle="Issues">
+    <h1>Issues</h1>
+    <IssueCard
+      issueCoverSrc = {Cover}
+      issueLink = "/white-dwarf-magazine-101"
+      issueNumber = "White Dwarf 101"
+      issueDate = "May 1988"
+    />
+  </Layout>
 )
