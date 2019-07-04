a6502d77e74d55680a565ecb422c21a4d14c4dba
Stuart Hicks <stuart.hicks@gmail.com>
2019-07-04 10:31:31 +0200

    The discalimer page plus linking it to the home page.

--- src/pages/disclaimer.js
+++ src/pages/disclaimer.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+
 export default () => (
-  <h1>Disclaimer</h1>
+  <Layout pageTitle="Disclaimer">
+    <h1>Disclaimer</h1>
+    <p>
+      This web site is completely unofficial and in no way endorsed by Games Workshop Limited.
+    </p>
+    <p>
+      Warhammer, Warhammer 40000, Horus Heresy, Warhammer Age of Sigmar, White Dwarf, and all associated marks, names, races, race insignia, characters, vehicles, locations, units, illustrations and images from the Warhammer Universe are either ®, TM and/or © Copyright Games Workshop Ltd 2000-2017, variably registered in the UK and other countries around the world. Used without permission. No challenge to their status intended. All Rights Reserved to their respective owners.
+    </p>
+    <p>
+      Now please return to the fun part of the website! 
+    </p>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -6,7 +7,7 @@ export default () => (
   <Layout pageTitle="White Library">
     <h1>The White Library - An Unofficial Database for White Dwarf Magazine</h1>
     <p>
-      Welcome to The White Library, an unofficial database for Games Workshop's White Dwarf magazine. If you're looking to locate a particular article or just want to take a nostalgic browse through old issues then please feel free to look around. The very nature of the website means it should never be complete (unless White Dwarf stops being published!) so if you'd like to contribute then feel free to help us out with an issue that hasn't currently been included, or alternatively feel free to just drop us a line to reminisce about your favourite issue. The best contributions may even find their way onto the website.
+      Welcome to The White Library, an <Link to="/disclaimer">unofficial</Link> database for Games Workshop's White Dwarf magazine. If you're looking to locate a particular article or just want to take a nostalgic browse through old issues then please feel free to look around. The very nature of the website means it should never be complete (unless White Dwarf stops being published!) so if you'd like to contribute then feel free to help us out with an issue that hasn't currently been included, or alternatively feel free to just drop us a line to reminisce about your favourite issue. The best contributions may even find their way onto the website.
     </p>
     <p>Have fun!</p>
   </Layout>
