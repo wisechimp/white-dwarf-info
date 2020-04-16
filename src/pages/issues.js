779f3fa9dda8499427fd9f8db6f80a25be77a01d
shicks1080 <stuart.hicks@gmail.com>
2020-04-16 16:05:52 +0200

    Updating the issues page
    Changing this from a class to functional component.
    Removing lodash.

--- src/pages/issues.js
+++ src/pages/issues.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react'
-import get from "lodash/get"
+import React from "react"
 import { graphql } from "gatsby"
 
-import IssueCard from '../components/issuecard'
-import Layout from '../components/layout'
+import IssueCard from "../components/issuecard"
+import Layout from "../components/layout"
 
-class Issues extends Component {
-  render() {
-    const issueData = get(this, 'props.data.allWhitedwarfdataJson.edges')
-
-    return (
-      <Layout pageTitle="Issues">
-        <h1>Issues</h1>
-        <div>
-          {issueData.map(({ node }) => {
-            return (
-              <IssueCard
-                key={node.issue}
-                issueCoverSrc = {node.coverSrc.childImageSharp.fixed}
-                issueCoverAltText = {"The cover of White Dwarf magazine issue " + node.issue}
-                issueLink = {"/white-dwarf-magazine-issue-" + node.issue + "/"}
-                issueNumber = {"White Dwarf " + node.issue}
-                issueDate = {node.date}
-              />
-            )
-          })}
-        </div>
-      </Layout>
-    )
-  }
-}
-
-export default Issues
+export default ({ data }) => (
+  <Layout pageTitle="Issues">
+    <h1>Issues</h1>
+    <div>
+      {data.allWhitedwarfdataJson.edges.map(({ node }) => {
+        return (
+          <IssueCard
+            key={node.issue}
+            issueCoverSrc={node.coverSrc.childImageSharp.fixed}
+            issueCoverAltText={
+              "The cover of White Dwarf magazine issue " + node.issue
+            }
+            issueLink={"/white-dwarf-magazine-issue-" + node.issue + "/"}
+            issueNumber={"White Dwarf " + node.issue}
+            issueDate={node.date}
+          />
+        )
+      })}
+    </div>
+  </Layout>
+)
 
 export const issuesQuery = graphql`
   query {
