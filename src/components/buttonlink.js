5b650975f5819e2a71c3722b303fc735911ed47d
shicks1080 <stuart.hicks@gmail.com>
2020-05-28 13:51:27 +0200

    Styling the previous and next issue buttons

--- src/components/buttonlink.js
+++ src/components/buttonlink.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import buttonStyles from "./buttonlink.module.css"
+
 export default props => (
-  <Link to={`/issues/white-dwarf-magazine-${props.targetIssueNumber}`}>
+  <Link
+    className={buttonStyles.buttonLink}
+    to={`/issues/white-dwarf-magazine-${props.targetIssueNumber}`}
+  >
     {props.targetIssueNumber}
   </Link>
 )
