d30d988d4a6e256192622c8833dc60223d1ebaa8
shicks1080 <stuart.hicks@gmail.com>
2020-05-29 15:14:27 +0200

    Adding Helmet
    Starting the head component

---
+++ src/components/head.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Helmet from "gatsby-plugin-react-helmet"
+
+export default props(
+  <Helmet title={props.title}>
+    <meta name="description" content="" />
+  </Helmet>
+)
