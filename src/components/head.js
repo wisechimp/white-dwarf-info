a47e48c59a80f5fe3786e06d95b443224ba1f5d4
shicks1080 <stuart.hicks@gmail.com>
2020-07-03 16:14:32 +0200

    Update head.js
    Removing the ebay "smart" placement script from the <head>

--- src/components/head.js
+++ src/components/head.js
@@ -1,11 +1,4 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-export default () => (
-  <Helmet>
-    <script
-      async
-      src="https://epnt.ebay.com/static/epn-smart-tools.js"
-    ></script>
-  </Helmet>
-)
+export default () => <Helmet></Helmet>
