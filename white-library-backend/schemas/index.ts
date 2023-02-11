70b76ca147ac90db5f0d1520971b371a79e914a5
Stuart Hicks <stuart.hicks@gmail.com>
2023-02-11 18:11:01 +0100

    feat: Adding issue schema

--- white-library-backend/schemas/index.ts
+++ white-library-backend/schemas/index.ts
@@ -1,6 +1,7 @@
 import blockContent from './blockContent'
+import issue from './issue'
 import category from './category'
 import post from './post'
 import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [issue, post, author, category, blockContent]
